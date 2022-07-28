699a56e5c34e0da152576eb14a9fb4a3bc480574
Toni Röyhy <toni@montel.fi>
2022-07-28 10:10:06 +0300

    generalize annotation joining

--- finBERT-NER.py
+++ finBERT-NER.py
@@ -1,8 +1,6 @@
-from functools import reduce
-
 import requests
-import json
 import pandas as pd
+import re
 
 
 # jsonista taulukko
@@ -21,40 +19,67 @@ def join_b_organisations(org_list):
     return orgs
 
 
+def join_annotations(ann_list):
+    """Joins different types of annotations to list of tuples [('<annotation>', '<type>')]"""
+
+    i_reg = re.compile(r'(.*)\tI-([A-Z]*)')
+    b_reg = re.compile(r'(.*)\tB-([A-Z]*)')
+
+    annotations = []
+    last_cat = None
+
+    for org in ann_list:
+        if match := b_reg.search(org):
+            name = match.group(1)
+            last_cat = cat = match.group(2)
+            annotations.append((name, cat))
+        elif match := i_reg.search(org):
+            name = match.group(1)
+            cat = match.group(2)
+            if last_cat == cat:
+                annotations[-1] = f'{annotations[-1][0]} {name}', annotations[-1][1]
+            else:
+                annotations.append((name, cat))
+
+    return annotations
+
+
 def main():
     df = pd.read_csv('S-ryhmä_tweet_sentiments.csv')
 
     # taulukosta otetaan 100 kpl otos
+    df2: pd.DataFrame = df
+    #   df2: pd.DataFrame = df.iloc[:10]
 
-    df2: pd.DataFrame = df.iloc[:99, :]
-    df2['B-ORG'] = None
+    df2['annotations'] = None
 
     pd.set_option('display.max_rows', df2.shape[0] + 1)
 
-    l = []
-
-    # l�het�n otoksen tekstit localhostiin
+    # regex for matching B-*** and I-*** annotations, used later
+    i_reg = re.compile(r'(.*)\tI-([A-Z]*)')
+    b_reg = re.compile(r'(.*)\tB-([A-Z]*)')
 
     for index, row in df2.iterrows():
         text = row['cleanContent']
 
+        # Query Finbert-NER
         response = requests.get('http://localhost:5000/', data=text.encode())
 
-        x = response.text.split("\n")
+        words = response.text.split("\n")
 
+        # filter entities that have annotations
         def filter_organisations(ent):
-            if 'B-ORG' in ent:
-                return True
-            if 'I-ORG' in ent:
-                return True
-            return False
+            return any([b_reg.match(ent), i_reg.match(ent)])
 
-        oganizations = filter(filter_organisations, x)
-        joined = join_b_organisations(oganizations)
+        annotations = filter(filter_organisations, words)
 
-        df2.at[index, 'B-ORG'] = joined
+        joined = join_annotations(annotations)
 
-    print(df2[['cleanContent', 'B-ORG']])
+        df2.at[index, 'annotations'] = joined
+
+    print(df2[['cleanContent', 'annotations']])
+
+    df2[['cleanContent', 'annotations']].to_csv('test.csv')
 
 
 if __name__ == '__main__':
